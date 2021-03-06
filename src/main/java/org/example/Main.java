3be994de5db70605c88488a6424f34e0b00fc02b
hasan <hasan1993mur@gmail.com>
2021-03-06 12:08:58 +0500

    небольшая доработка

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,13 +7,14 @@ public class Main {
         int b = 0;
         Integer[] a=new Integer[3];
         a[0]=null;
-        a[4]=0;
+        a[3]=0;
         a[2]=1;
         System.out.println(a[0].toString());
         System.out.println(a[4]);
         try {
+
             a[1]=1/b;
-            if (b>0) throw new ArithmeticException();
+            if (b == 0) throw new ArithmeticException();
         }catch (ArithmeticException ar){
             System.out.println("на ноль делить нельзя");
         }
