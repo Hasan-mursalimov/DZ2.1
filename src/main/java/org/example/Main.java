45dc2bbc2d50e49afbf57873698ce32d69c76714
hasan <hasan1993mur@gmail.com>
2021-03-09 16:11:28 +0500

    Изменено

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,29 @@
 package org.example;
 
 
-
 public class Main {
-    public static void main(String[] args) {
-        int b = 0;
-        Integer[] a=new Integer[3];
-        a[0]=null;
-        a[3]=0;
-        a[2]=1;
-        System.out.println(a[0].toString());
-        System.out.println(a[4]);
-        try {
 
-            a[1]=1/b;
-            if (b == 0) throw new ArithmeticException();
-        }catch (ArithmeticException ar){
-            System.out.println("на ноль делить нельзя");
+
+    public static void main(String[] args) {
+        Integer[] a = new Integer[3];
+        try {
+            a[3] = 1;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: " + e.getMessage());
         }
+        try {
+            a[0] = null;
+            System.out.println(a[0].toString());
+        } catch (NullPointerException n) {
+            System.out.println("Error: " + n.getMessage());
+        }
+        try {
+            int b = 0;
+            a[1] = 1 / b;
+        } catch (ArithmeticException ar) {
+            System.out.println("Error:"+ar.getMessage());
+        }
+
 
     }
 }
